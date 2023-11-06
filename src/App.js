cd4657fc7a9e427180ca16c402169aa86aac6e94
thanhtr297 <thanh.tranminh.hanoi@gmail.com>
2023-11-06 11:01:07 +0700

    push template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// react router v6
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+// pages
+import {Home, CategoryProduct, ProductSingle, Cart, Search} from "./pages/index";
+// components
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Footer from "./components/Footer/Footer";
+import store from "./store/store";
+import {Provider} from "react-redux";
+// import Login from "./components/Login/Login";
+import {useState} from "react";
+
 
 function App() {
+    // const [check, setCheck] = useState(true)
+    // const test = () => {
+    //     setCheck(false)
+    // }
+    // const test1 = () => {
+    //     setCheck(true)
+    // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store = {store}>
+        <BrowserRouter>
+
+          {/*{check && <Header a={test}/>}*/}
+          {/*{!isLoginPage && <Sidebar />}*/}
+            <Header/>
+          <Sidebar />
+          <Routes>
+            {/* home page route */}
+            <Route path = "/" element = {<Home />} />
+            {/* single product route */}
+            <Route path = "/product/:id" element = {<ProductSingle />} />
+            {/* category wise product listing route */}
+            <Route path = "/category/:category" element = {<CategoryProduct />} />
+            {/* cart */}
+            <Route path = "/cart" element = {<Cart />} />
+            {/* searched products */}
+            <Route path = "/search/:searchTerm" element = {<Search />} />
+            {/*<Route path={"/login"} element={<Login b={test1}/>}/>*/}
+          </Routes>
+
+          <Footer/>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import "./Navbar.scss";
+import {Link} from "react-router-dom";
+import { useSelector, useDispatch} from 'react-redux';
+import { setSidebarOn } from '../../store/sidebarSlice';
+import { getAllCategories } from '../../store/categorySlice';
+import { getAllCarts, getCartItemsCount, getCartTotal } from '../../store/cartSlice';
+import CartModal from "../CartModal/CartModal";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const categories = useSelector(getAllCategories);
+  const carts = useSelector(getAllCarts);
+  const itemsCount = useSelector(getCartItemsCount);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  }
+
+  useEffect(() => {
+    dispatch(getCartTotal());
+  }, [carts])
+
+  return (
+    <nav className='navbar1'>
+      <div className='navbar1-cnt flex align-center'>
+        <div className='brand1-and-toggler flex align-center'>
+          <button type = "button" className='sidebar-show-btn text-white' onClick={() => dispatch(setSidebarOn())}>
+            <i className='fas fa-bars'></i>
+          </button>
+          <Link to = "/" className='navbar1-brand flex align-center'>
+            <span className='navbar1-brand-ico'>
+              <i className='fa-solid fa-bag-shopping'></i>
+            </span>
+            <span className='navbar1-brand-txt mx-2'>
+              <span className='fw-7'>Snap</span>Up.
+            </span>
+          </Link>
+        </div>
+
+        <div className='navbar1-collapse w-100'>
+          <div className='navbar1-search bg-white'>
+            <div className='flex align-center'>
+              <input type = "text" className='form1-control fs-14' placeholder='Search your preferred items here' onChange={(e) => handleSearchTerm(e)} />
+              <Link to = {`search/${searchTerm}`} className='text-white search-btn flex align-center justify-center'>
+                  <i className='fa-solid fa-magnifying-glass'></i>
+                </Link>
+            </div>
+          </div>
+
+          <ul className='navbar1-nav flex align-center fs-12 fw-4 font-manrope'>
+            {
+              // taking only first 8 categories
+              categories.slice(0, 8).map((category, idx) => (
+                <li className='nav1-item no-wrap' key = {idx}>
+                  <Link to = {`category/${category}`} className='nav1-link text-capitalize'>{category.replace("-", " ")}</Link>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+
+        <div className='navbar1-cart flex align-center'>
+          <Link to = "/cart" className='cart-btn'>
+            <i className='fa-solid fa-cart-shopping'></i>
+            <div className='cart1-items-value'>{itemsCount}</div>
+            <CartModal carts = {carts} />
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
