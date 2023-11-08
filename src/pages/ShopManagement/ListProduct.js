9e68cc25c71482d6a54fcbd851cc7d18919208dc
VietAnhNguyen9 <vietanh260396@gmail.com>
2023-11-08 15:10:45 +0700

    FE-Create done

--- src/pages/ShopManagement/ListProduct.js
+++ src/pages/ShopManagement/ListProduct.js
@@ -3,19 +3,10 @@ import { Button } from 'react-bootstrap';
 import CreateProduct from './CreateProduct'; // Giả sử CreateProduct và ListProduct cùng folder
 
 function ListProduct() {
-    const [showCreateModal, setShowCreateModal] = useState(false);
-
-    const handleShowCreateModal = () => setShowCreateModal(true);
-    const handleCloseCreateModal = () => setShowCreateModal(false);
 
     return (
         <>
-            <Button variant="primary" onClick={handleShowCreateModal}>
-                Tạo Sản Phẩm Mới
-            </Button>
-
-            {/* Modal từ component CreateProduct */}
-            <CreateProduct show={showCreateModal} handleClose={handleCloseCreateModal} />
+            <CreateProduct/>
         </>
     );
 }
