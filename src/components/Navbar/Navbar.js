66bd8d2779283f45e8cf3353ca07592960c7e903
chien2002 <chienad2002ad@gmail.com>
2023-11-15 21:47:27 +0700

    sửa lỗi đăng nhập

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -37,35 +37,35 @@ const Navbar = () => {
           <button type = "button" className='sidebar-show-btn text-white' onClick={() => dispatch(setSidebarOn())}>
             <i className='fas fa-bars'></i>
           </button>
-          <Link to = "/" className='navbar1-brand flex align-center'>
+          <Link to = "/" className='navbar1-brand flex align-center' style={{marginRight : '0px'}}>
             <span className='navbar1-brand-ico'>
               <i className='fa-solid fa-bag-shopping'></i>
             </span>
-            <span className='navbar1-brand-txt mx-2'>
-              <span className='fw-7'>Snap</span>Up.
+            <span className='navbar1-brand-txt mx-2' style={{fontSize : '18px' ,width : '100px'}}>
+              <span className='fw-7' >Mua Gì Đây</span>
             </span>
           </Link>
         </div>
 
-        <div className='navbar1-collapse w-100'>
+        <div className='navbar1-collapse w-100'style={{paddingTop: '15px' }}>
           <div className='navbar1-search bg-white'>
-            <div className='flex align-center'>
-              <input type = "text" className='form1-control fs-14' placeholder='Search your preferred items here' onChange={(e) => handleSearchTerm(e)} />
+            <div className='flex align-center' >
+              <input type = "text" className='form1-control fs-14' placeholder='Tìm kiếm' onChange={(e) => handleSearchTerm(e)} />
               <Link to = {`search/${searchTerm}`} className='text-white search-btn flex align-center justify-center'>
                   <i className='fa-solid fa-magnifying-glass'></i>
                 </Link>
             </div>
           </div>
 
-          <ul className='navbar1-nav flex align-center fs-12 fw-4 font-manrope'>
-            {
-              // taking only first 8 categories
-              categories.slice(0, 8).map((category, idx) => (
-                <li className='nav1-item no-wrap' key = {idx}>
-                  <Link to = {`category/${category}`} className='nav1-link text-capitalize'>{category.replace("-", " ")}</Link>
-                </li>
-              ))
-            }
+          <ul className='navbar1-nav flex align-center fs-12 fw-4 font-manrope' style={{paddingBottom: "10px"}}>
+            {/*{*/}
+            {/*  // taking only first 8 categories*/}
+            {/*  categories.slice(0, 8).map((category, idx) => (*/}
+            {/*    <li className='nav1-item no-wrap' key = {idx}>*/}
+            {/*      <Link to = {`category/${category}`} className='nav1-link text-capitalize'>{category.replace("-", " ")}</Link>*/}
+            {/*    </li>*/}
+            {/*  ))*/}
+            {/*}*/}
           </ul>
         </div>
 
