2c97ba462ca44072ff9c786895bad482b59ef5e0
VietAnhNguyen9 <vietanh260396@gmail.com>
2023-11-20 14:30:19 +0700

    bieu do

--- src/pages/ShopManagement/Report.js
+++ src/pages/ShopManagement/Report.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
 
-function Report() {
+export default function Report() {
     return (
-        <div>
-            report
-        </div>
+        <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+                {
+                    data: [2, 5.5, 2, 8.5, 1.5, 5],
+                },
+            ]}
+            width={500}
+            height={300}
+        />
     );
 }
-
-export default Report;
